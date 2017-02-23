de5a66c3fb8330ed5737eb248f575191f81a8c0e
U-BSD\293359 <293359@SHS-10DD3C387.beaverton.k12.or.us>
2017-02-23 14:30:34 -0800

    Created Heap.cpp and input/output

---
+++ Heap.cpp
@@ -0,0 +1,20 @@
+/*
+ * Heap.cpp
+ *
+ *  Created on: Feb 23, 2017
+ *      Author: Kevin Jin
+ */
+
+#include <iostream>
+#include <stdio.h>
+
+
+int main() {
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+
+
+
+	return 0;
+}
+

6dbb294b4bb8925bd8db37e8ec2391b9c47c180e
U-BSD\293359 <293359@SHS-10DD3C387.beaverton.k12.or.us>
2017-03-01 14:31:26 -0800

    Finished project for file, now do the same for console and add comments

--- Heap.cpp
+++ Heap.cpp
@@ -8,12 +8,17 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <math.h>
 
 using namespace std;
-
+void up_heap(int* & heap, int pos);
+void pop_off(int* & heap, int iterator);
+void down_heap(int* & heap, int pos, int iterator);
 int main () {
 	bool valid = false;
 	while (!valid) {
+		int* heap = new int[101];
+		int iterator = 0;
 		char choose;
 		cout << "Do you want to enter the numbers through the console or through a file?" << endl << "Choose: ";
 		cin >> choose;
@@ -28,6 +33,7 @@ int main () {
 				char* nums = new char[501];
 				if (!input.eof()) {
 					input.getline(nums, 500);
+					cout << "File contents:" << endl;
 					cout << nums;
 					int temp = 0;
 					for (int i = 0; i < strlen(nums); i++) {
@@ -36,12 +42,23 @@ int main () {
 							temp += nums[i] - '0';
 						}
 						else {
-							cout << endl << temp;
+							heap[iterator] = temp;
+							up_heap(heap, iterator);
+							//cout << endl << temp;
 							temp = 0;
+							iterator++;
 						}
 					}
-					cout << endl << temp;
-
+					heap[iterator] = temp;
+					up_heap(heap, iterator);
+					//cout << endl << temp;
+					temp = 0;
+					//cout << endl << temp;
+					cout << endl << "After sort:" << endl;
+//					for (int i = 0; i < 100; i++) {
+//						cout << " " << heap[i];
+//					}
+					pop_off(heap, iterator);
 
 				}
 
@@ -62,4 +79,59 @@ int main () {
 	return 0;
 }
 
+void up_heap(int* & heap, int pos) {
+	if (pos != 0) {
+		int parent = (pos - 2 + pos % 2) / 2;
+		if (heap[parent] < heap[pos]) {
+			int temp = heap[parent];
+			heap[parent] = heap[pos];
+			heap[pos] = temp;
+			up_heap(heap, parent);
+		}
+	}
+}
 
+void pop_off(int* & heap, int iterator) {
+	if (iterator > 0) {
+		cout << heap[0] << " ";
+		heap[0] = heap[iterator];
+		iterator--;
+		down_heap(heap, 0, iterator);
+		pop_off(heap, iterator);
+	}
+	else
+		cout << heap[0] << " ";
+}
+
+void down_heap(int* & heap, int pos, int iterator) {
+	if ((int)log2(pos + 1) < (int)log2(iterator + 1)) {
+		if (heap[pos] < heap[pos * 2 + 1]) {
+			if (heap[pos] < heap[pos * 2 + 2]) {
+				if (heap[pos * 2 + 1] > heap[pos * 2 + 2]) {
+					int temp = heap[pos];
+					heap[pos] = heap[pos * 2 + 1];
+					heap[pos * 2 + 1] = temp;
+					down_heap(heap, pos * 2 + 1, iterator);
+				}
+				else {
+					int temp = heap[pos];
+					heap[pos] = heap[pos * 2 + 2];
+					heap[pos * 2 + 2] = temp;
+					down_heap(heap, pos * 2 + 2, iterator);
+				}
+			}
+			else {
+				int temp = heap[pos];
+				heap[pos] = heap[pos * 2 + 1];
+				heap[pos * 2 + 1] = temp;
+				down_heap(heap, pos * 2 + 1, iterator);
+			}
+		}
+		else if (heap[pos] < heap[pos * 2 + 2]) {
+			int temp = heap[pos];
+			heap[pos] = heap[pos * 2 + 2];
+			heap[pos * 2 + 2] = temp;
+			down_heap(heap, pos * 2 + 2, iterator);
+		}
+	}
+}
